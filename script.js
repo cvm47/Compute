2aa32c0fb5d99d8a3e36373c0125c8aa83048c27
cvm47 <shivamuniyal1810@gmail.com>
2025-09-23 10:20:20 +0530

    feat: add operate function to handle arithmetic based on operator

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b
 }
+
+let num1, operator, num2;
+
+function operate(operator, num1 ,num2) {
+    if (operator === '+'){
+        return add (num1, num2)
+    }
+    else if (operator === '-'){
+        return subtract (num1, num2)
+    }
+    else if (operator === '*') {
+        return multiply(num1, num2)
+    }
+    else if (operator === '/') {
+        return divide(num1, num2)
+    }
+}
